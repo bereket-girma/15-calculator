559e64aa82b69fa3bc0ebcd8e100e348331d845e
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-09-16 02:39:39 -0700

    message

---
+++ main.js
@@ -0,0 +1,9 @@
+// let calculator = document.querySelector(".calculator") 
+function calcNumbers(result) {
+    form.displayResult.value = form.displayResult.value + result;
+  }
+  
+  function myFunction() {
+    document.getElementById("myForm").reset();
+  }
+  
